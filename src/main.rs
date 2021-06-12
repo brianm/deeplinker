3e3454aa562886a5083aa8cbcf7d45d14688cb38
Brian McCallister <brianm@skife.org>
2021-06-12 10:44:20 -0700

    formatting

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,12 @@ use std::io::Write;
 use std::process::{Command, Stdio};
 use std::str::from_utf8;
 fn main() -> Result<(), Box<dyn Error>> {
-    
     let app_name = front_app()?;
     println!("'{}'", app_name);
 
     let output = match &app_name[..] {
         "Safari" | "Webkit" => safari()?,
-        "Google Chrome" |"Google Chrome Canary" | "Chromium" => chrome()?,
+        "Google Chrome" | "Google Chrome Canary" | "Chromium" => chrome()?,
         _ => String::from("unknown application"),
     };
 
@@ -39,24 +38,24 @@ fn main() -> Result<(), Box<dyn Error>> {
 macro_rules! scripty {
     ($($name:ident),*) => {
         $(
-        fn $name() -> Result<String, Box<dyn Error>> {
-            let script = include_str!(concat!("scripts/", stringify!($name), ".applescript"));
-            let mut child = Command::new("/usr/bin/osascript")
-                .arg("-")
-                .stdin(Stdio::piped())
-                .stdout(Stdio::piped())
-                .spawn()?;
-            // .expect("failed to execute process");
-            let child_stdin = child.stdin.as_mut().unwrap();
-            child_stdin.write_all(script.as_bytes())?;
-            // Close stdin to finish and avoid indefinite blocking
-            drop(child_stdin);
-        
-            let output = child.wait_with_output()?;
-            let out = output.stdout;
-            Ok(String::from(from_utf8(&out)?.trim()))
-        }
-    )*
+            fn $name() -> Result<String, Box<dyn Error>> {
+                let script = include_str!(concat!("scripts/", stringify!($name), ".applescript"));
+                let mut child = Command::new("/usr/bin/osascript")
+                    .arg("-")
+                    .stdin(Stdio::piped())
+                    .stdout(Stdio::piped())
+                    .spawn()?;
+                // .expect("failed to execute process");
+                let child_stdin = child.stdin.as_mut().unwrap();
+                child_stdin.write_all(script.as_bytes())?;
+                // Close stdin to finish and avoid indefinite blocking
+                drop(child_stdin);
+
+                let output = child.wait_with_output()?;
+                let out = output.stdout;
+                Ok(String::from(from_utf8(&out)?.trim()))
+            }
+        )*
     };
 }
 
