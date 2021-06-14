4671e339ba68a09e0f39c535fd803fd0a76b401a
Brian McCallister <brianm@skife.org>
2021-06-13 21:10:54 -0700

    fmt

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::error::Error;
 use deeplinker::scripts;
 fn main() -> Result<(), Box<dyn Error>> {
     let app_name: String = scripts::front_app()?;
-    println!("{}",app_name);
+    println!("{}", app_name);
 
     let output: scripts::DeepLink = match &app_name[..] {
         "Safari" | "Webkit" => scripts::safari()?,
--- src/scripts/mod.rs
+++ src/scripts/mod.rs
@@ -1,21 +1,21 @@
+use json5;
+use serde::de::DeserializeOwned;
+use serde::{Deserialize, Serialize};
 use std::error::Error;
 use std::io::Write;
 use std::process::{Command, Stdio};
-use serde::de::DeserializeOwned;
-use serde::{Serialize, Deserialize};
-use json5;
 use std::str::from_utf8;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct DeepLink {
     pub link: String,
-    pub title: String
+    pub title: String,
 }
 
 macro_rules! scripty {
     ($($name:ident),*) => {
         $(
-            pub fn $name<T>() -> Result<T, Box<dyn Error>> 
+            pub fn $name<T>() -> Result<T, Box<dyn Error>>
                 where T: DeserializeOwned
             {
                 let script = include_str!(concat!(stringify!($name), ".applescript"));
